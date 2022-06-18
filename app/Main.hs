b5175abc9ef00094998fa2bbb924254c2a3ce019
TrueWarg <irrmm9000@gmail.com>
2022-06-18 16:53:10 +0200

    Integrate yeasod

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,25 @@
+{-# LANGUAGE OverloadedStrings, QuasiQuotes, TemplateHaskell, TypeFamilies #-}
+
 module Main where
 
-import Lib
+import Yesod
+
+data App = App -- Put your config, database connection pool, etc. in here.
+
+-- Derive routes and instances for App.
+mkYesod "App" [parseRoutes|
+/ HomeR GET
+/page1 Page1R GET
+/page2 Page2R GET
+|]
+
+instance Yesod App -- Methods in here can be overridden as needed.
+
+-- The handler for the GET request at /, corresponds to HomeR.
+getHomeR :: Handler Html
+getHomeR = defaultLayout [whamlet|Lol Kek Chebureck|]
+getPage1R = defaultLayout [whamlet|<a href=@{Page2R}>Go to page 2|]
+getPage2R = defaultLayout [whamlet|<a href=@{HomeR}>Go home|]
 
 main :: IO ()
-main = someFunc
+main = warp 3000 App
